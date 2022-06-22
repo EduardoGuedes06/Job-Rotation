cc9514b18e50eeab7e6630e3af957556ef222019
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-06-22 10:57:32 -0300

    1.3

--- Atividades/A3.py
+++ Atividades/A3.py
@@ -1,51 +1,64 @@
 import json
+from typing import Dict
 
-from numpy import append
-from collections import defaultdict
+d=[]
+m=[]
+f=[]
+mes1_max=0
+mes2_max=0
 
-with open("Fatura.json", encoding='utf-8') as meu_json:
-    dados = json.load(meu_json)
 
-Ldias_mes = []
-Lfaturamento_mes = []
 
-Ldias_mesT = []
-Lfaturamento_mesT = []
-
-Ldias_mesT = []
-Lfaturamento_mesT = []
-
-Faturamento_max = 0
-aux = 0
+meu_json=open("Fatura.json", encoding='utf-8')
+dados = json.load(meu_json)
 
 
 # para cada item do arquivo 
 def Inserir():
     for i in dados:
      
-        Ldias_mes.append(i['dia'])
-        Lfaturamento_mes.append(i['faturamento'])
+        #print(i["dia"])
+        m.append(i['mes'])
+        d.append(i["dia"])
+        f.append(i['faturamento'])
+  
 
 
 if __name__ == "__main__":
     Inserir()
+    fatura=[]
+    for element in f:
+        if element == '':
+           
+            fatura.append('Nulo')
+            
+        else:
+            fatura.append(int(element))
+            print(fatura)
     
-    print(Lfaturamento_mes)
+    for i in range (59):
+        
+        if fatura[i] == int:  
+            print()
+            if m[1]:      
+                   
+                        
+                if fatura[i] > mes1_max:
+                    mes1_max = fatura[i]
+                    print(mes1_max)
+                    print("\nFatura do mes 1, dia",d[i],":",fatura[i])
+
+            if m[2]:     
+                   
+                   
+                        
+                if fatura[i] > mes2_max:
+                    mes2_max = fatura[i]
+     
 
 
-
-    
-    
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
+                
+                
+        else: fatura[i] == 'Nulo'
+   
+ 
